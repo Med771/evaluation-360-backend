ac0a113d23653693b014673952291b62b0dea9ea
Med771 <mm7737333@gmail.com>
2025-02-25 14:42:46 +0300

    Start project

---
+++ src/main/java/ru/singularity/evaluation360/Evaluation360Application.java
@@ -0,0 +1,11 @@
+package ru.singularity.evaluation360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluation360Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Evaluation360Application.class, args);
+    }
+}
