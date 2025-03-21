cef60cf16d29f64ddfa89029a08706e08fe8614e
Med771 <mm7737333@gmail.com>
2025-03-21 14:16:21 +0300

    Update and create Entity

---
+++ src/main/java/ru/singularity/evaluation360/entity/ResultEntity.java
@@ -0,0 +1,34 @@
+package ru.singularity.evaluation360.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import ru.singularity.evaluation360.dto.result.model.SkillsResultModel;
+
+import java.util.List;
+
+@Document
+@Getter
+@Setter
+public class ResultEntity {
+    @Id
+    private String id;
+
+    private String title;
+
+    // Average Result
+    private Double averageResult;
+
+    // Results
+    private Double thisResult;
+    private Double commandResult;
+    private Double expertResult;
+
+    // Skills results
+    private List<SkillsResultModel> results;
+
+    // Final comment
+    private String comment;
+}
