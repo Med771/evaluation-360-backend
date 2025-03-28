e04acc35e42c1da93d72647269996501bc7d31a2
Med77_1 <mm7737333@gmail.com>
2025-03-28 11:08:13 +0300

    create DTO for admin panel

--- src/main/java/ru/singularity/evaluation360/dto/test/QuestionsResponseDTO.java
+++ src/main/java/ru/singularity/evaluation360/dto/test/QuestionsResponseDTO.java
@@ -4,6 +4,6 @@ import ru.singularity.evaluation360.dto.test.model.QuestionModel;
 
 import java.util.List;
 
-public record QuestionResponseDTO(
+public record QuestionsResponseDTO(
         List<QuestionModel> questions) {
 }
---
+++ src/main/java/ru/singularity/evaluation360/dto/test/TestEditResponseDTO.java
@@ -0,0 +1,27 @@
+package ru.singularity.evaluation360.dto.test;
+
+import ru.singularity.evaluation360.dto.respondent.model.RespondentModel;
+import ru.singularity.evaluation360.dto.test.model.QuestionModel;
+import ru.singularity.evaluation360.entity.model.StatusTestEnum;
+import ru.singularity.evaluation360.entity.model.TypeTestEnum;
+
+import java.util.List;
+
+public record TestEditResponseDTO(
+        String title,
+
+        TypeTestEnum type,
+        StatusTestEnum status,
+
+        Long createTimeStamp,
+        Long startTimeStamp,
+        Long endTimeStamp,
+
+        List<RespondentModel> respondents,
+        List<QuestionModel> questions,
+
+        Integer minRespondents,
+        Integer maxRespondents,
+        Integer minHighRoleRespondents,
+        Integer minOtherCourseRespondents) {
+}
