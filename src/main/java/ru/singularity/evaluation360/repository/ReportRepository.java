e81f55dc31ae857ccd610f9e22fede73fa37afca
daniil <daniltihomirov946@gmail.com>
2025-04-17 23:33:28 +0300

    4 step add services

--- src/main/java/ru/singularity/evaluation360/repository/ReportRepository.java
+++ src/main/java/ru/singularity/evaluation360/repository/ReportRepository.java
@@ -5,8 +5,10 @@ import ru.singularity.evaluation360.entity.ReportEntity;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ReportRepository extends MongoRepository<ReportEntity, String> {
     List<ReportEntity> findByTestIdAndEvaluatorIdAndEvaluatedIdIn(String testId, Integer evaluatorId, Collection<Integer> evaluatedId);
     List<ReportEntity> findByTestIdAndEvaluatorIdInAndEvaluatedId(String testId, Collection<Integer> evaluatorId, Integer evaluatedId);
+    Optional<ReportEntity> findByEvaluatedIdTestIdEvaluatorId(String evaluatedIdTestIdEvaluatorId);
 }
--- src/main/java/ru/singularity/evaluation360/service/TestService.java
+++ src/main/java/ru/singularity/evaluation360/service/TestService.java
@@ -7,6 +7,7 @@ import ru.singularity.evaluation360.dto.test.model.QuestionTestModel;
 import ru.singularity.evaluation360.dto.test.model.TestRespondentTitleModel;
 import ru.singularity.evaluation360.dto.test.model.TestTitleModel;
 import ru.singularity.evaluation360.entity.*;
+import ru.singularity.evaluation360.entity.model.TypeTestEnum;
 import ru.singularity.evaluation360.exeptions.DontFoundException;
 import ru.singularity.evaluation360.mapper.ParticipantsMapper;
 import ru.singularity.evaluation360.mapper.TestMapper;
@@ -24,13 +25,27 @@ public class TestService {
     private final ParticipantsMapper participantsMapper;
     private final QuestionRepository questionRepository;
     private final ReportRepository reportRepository;
+    private final String splitter = "!_!*!_!";
 
-    private List<TestRespondentTitleModel> getEvaluatedRespondents(String testId, int userId, List<Integer> evaluated){
+    /**
+     * метод для нахождения TestRespondentTitleModel с полем оценил ли его пользователь
+     * @param testId id теста
+     * @param userId id пользователя
+     * @param evaluated список id тех кого должен оценить пользователь
+     * @return список TestRespondentTitleModel с полем оценил ли он пользователя
+     */
+    private List<TestRespondentTitleModel> getEvaluatedRespondents(String testId, int userId, List<Integer> evaluated) {
+        // нахождение всех репортов которые соответствуют условию
+        // testId == testId and elevatorId == userId and evaluatedId in evaluated
+        // для того чтобы найти репорты в которых пользователь оценил кого то и далее вытянуть из них кого он оценил
         List<ReportEntity> reportEntities = reportRepository.
                 findByTestIdAndEvaluatorIdAndEvaluatedIdIn(testId, userId, evaluated);
 
+        // список id кого пользователь оценил
         List<Integer> evaluatedIds = reportEntities.stream().map(ReportEntity::getEvaluatedId).toList();
 
+
+        // поиск ParticipantEntity которых оценил пользователь и всех
         List<ParticipantEntity> evaluatesComplete = participantRepository.findAllById(evaluatedIds);
         List<ParticipantEntity> evaluatesAll = participantRepository.findAllById(evaluated);
 
@@ -38,17 +53,29 @@ public class TestService {
 
     }
 
+    /**
+     * метод для нахождения кто оценил кого оценил а кто кого не оценил
+     * @param evaluatorsComplete те кто оценили кого то или их оценили
+     * @param evaluatorsAll все кого должны оценить или те кто должен оценить
+     * @return список TestRespondentTitleModel где указано выполнил ли он оценку
+     */
     private List<TestRespondentTitleModel> generateTestRespondentModels(List<ParticipantEntity> evaluatorsComplete,
-                                                                        List<ParticipantEntity> evaluatorsAll){
+                                                                        List<ParticipantEntity> evaluatorsAll) {
+
+        // создания сета для дальнейшего нахождения всех кто не оценил
         Set<ParticipantEntity> incompleteParticipants = new HashSet<>(evaluatorsAll);
         evaluatorsComplete.forEach(incompleteParticipants::remove);
+
+        // создания списка для добавления
         List<TestRespondentTitleModel> testRespondentTitleModels = new ArrayList<>();
 
+        // те кто оценил
         for (ParticipantEntity participantEntity : evaluatorsComplete) {
             testRespondentTitleModels.add(participantsMapper.
                     toTestRespondentTitleModel(participantEntity, true));
         }
 
+        // те кто не оценил
         for (ParticipantEntity participantEntity : incompleteParticipants) {
             testRespondentTitleModels.add(participantsMapper.
                     toTestRespondentTitleModel(participantEntity, false));
@@ -57,7 +84,17 @@ public class TestService {
         return testRespondentTitleModels;
     }
 
-    private List<TestRespondentTitleModel> getEvaluatorRespondents(String testId, int userId, List<Integer> evaluator){
+    /**
+     * метод для нахождения TestRespondentTitleModel с полем оценил ли он пользователя
+     * @param testId тест id
+     * @param userId id пользователя
+     * @param evaluator id оценщиков
+     * @return список TestRespondentTitleModel с указанием оценил ли он пользователя
+     */
+    private List<TestRespondentTitleModel> getEvaluatorRespondents(String testId, int userId, List<Integer> evaluator) {
+        // нахождение всех репортов которые соответствуют условию
+        // testId == testId and elevatorId in evaluator and evaluatedId == userId
+        // для того чтобы найти репорты в которых пользователя оценили
         List<ReportEntity> reportEntities = reportRepository.
                 findByTestIdAndEvaluatorIdInAndEvaluatedId(testId, evaluator, userId);
         List<Integer> evaluatorIds = reportEntities.stream().map(ReportEntity::getEvaluatorId).toList();
@@ -77,8 +114,8 @@ public class TestService {
     }
 
     private boolean isAllCompeted(List<TestRespondentTitleModel> testRespondentTitleModels) {
-        for(TestRespondentTitleModel testRespondentTitleModel : testRespondentTitleModels){
-            if (!testRespondentTitleModel.isComplete()){
+        for (TestRespondentTitleModel testRespondentTitleModel : testRespondentTitleModels) {
+            if (!testRespondentTitleModel.isComplete()) {
                 return false;
             }
         }
@@ -90,39 +127,62 @@ public class TestService {
         return new TestsResponseDTO("360", tests);
     }
 
+    /**
+     * получение и формирование TestMenu
+     * @param testId id теста
+     * @param userId id пользователя
+     * @return TestMenuResponseDTO
+     */
     public TestMenuResponseDTO getTestMenu(String testId, int userId) {
-        String index = String.format("%s!_|*|_!%d", testId, userId);
+        // формирование составного индекса и получение по нему EvaluationEntity
+        String index = String.format("%s%s%d", testId, splitter, userId);
         EvaluationEntity evaluation = evaluationRepository.findByIndex(index)
                 .orElseThrow(() -> new DontFoundException(String.format("not found test menu %s", index)));
 
+        // списки id тех кто оценивает и тех кого оценивают
         List<Integer> evaluatorIds = evaluation.getEvaluator();
         List<Integer> evaluatedIds = evaluation.getEvaluated();
 
+        TestEntity testEntity = testRepository.findById(testId).orElseThrow(() -> new DontFoundException(testId));
+
+        // получение флага нужно ли выбирать кого оценивать
+        boolean isGetRespondents = !(testEntity.getType() == TypeTestEnum.SELF);
+
+        // формирование составного индекса и получение флага оценил ли пользователь сам себя по репорту
+        String reportIndex = String.format("%d%s%d",userId, splitter, userId);
+
+        boolean isCompleteEvaluation = reportRepository.findByEvaluatedIdTestIdEvaluatorId(reportIndex).isPresent();
+
+        // TODO isActiveResult пока оставить true потом дописать логику
+        boolean isActiveResult = true;
+
+        // составление списков оценщиков и оцениваемых с моделью TestRespondentTitleModel
+        // которая учитывает выполнил ли он тест
         List<TestRespondentTitleModel> evaluator = new ArrayList<>();
         List<TestRespondentTitleModel> evaluated = new ArrayList<>();
-        //TODO не совсем понял какой флаг в TestMenuResponseDTO отвечает за отборожение тех кого мне надо оценить
-        //TODO isGetRespondents проверить по типу если self тогда фолс
-        // TODO isCompleteEvaluation проверить оценил ли сам себя через репорты
-        // TODO isActiveResult пока оставить true потом дописать логику
+
+        // выбраны ли оцениваемые
         boolean isSelectRespondents;
-        if (!evaluatorIds.isEmpty()){
+        if (!evaluatorIds.isEmpty()) {
             evaluator = getEvaluatorRespondents(testId, userId, evaluatorIds);
             isSelectRespondents = true;
         } else {
             isSelectRespondents = false;
         }
 
-        if (!evaluatedIds.isEmpty()){
+        if (!evaluatedIds.isEmpty()) {
             evaluated = getEvaluatedRespondents(testId, userId, evaluatedIds);
         }
 
+        // проверка все ли выполнили свои задачи
         boolean isCompeteEvaluator = isAllCompeted(evaluator);
         boolean isCompeteEvaluated = isAllCompeted(evaluated);
 
 
-
-        // TODO получить флаги
-        return null;
+        return new TestMenuResponseDTO(testEntity.getTitle(), isGetRespondents,
+                isSelectRespondents, isCompleteEvaluation,
+                isCompeteEvaluated, isCompeteEvaluator,
+                evaluated, evaluator, isActiveResult);
     }
 
     public TestResponseDTO getTest(String testID, long userId, long evaluatedId) {
@@ -137,24 +197,36 @@ public class TestService {
                 testMapper.toQuestionModelList(questionRepository.findAllById(testEntity.getQuestionsIds())));
     }
 
+    /**
+     * составление правильного порядка вопросов с сохранением их
+     * @param testRequestDTO дто в котором есть вопросы
+     * @return лист с id вопросов в правильном порядке
+     */
     private List<String> generateQuestionsIds(TestRequestDTO testRequestDTO) {
+
+        // составляем массив в котором будем располагать вопросы
         int totalSize = testRequestDTO.questionIds().size() + testRequestDTO.newQuestions().size();
         String[] ids = new String[totalSize];
 
+        // делаем 2 мапы первая с новыми вопросами вторая с уже используемыми
         Map<Integer, QuestionTestModel> newQuestions = testRequestDTO.newQuestions();
         Map<Integer, String> existingIds = testRequestDTO.questionIds();
 
+        // список для сохранения пакетом
         List<QuestionEntity> entitiesToSave = new ArrayList<>();
+        // проходимся по мапе попутно заполняя временное поле которое указывает порядковый номер
         for (Map.Entry<Integer, QuestionTestModel> entry : newQuestions.entrySet()) {
             QuestionEntity entity = testMapper.toQuestionEntity(entry.getValue());
             entity.setOriginalIndex(entry.getKey());
             entitiesToSave.add(entity);
         }
 
+        // сохраняем
         List<QuestionEntity> savedEntities = questionRepository.saveAll(entitiesToSave);
 
         savedEntities.sort(Comparator.comparingInt(QuestionEntity::getOriginalIndex));
 
+        // 2 цикла по старым и новым id которые выставляют их в списке по их порядковому номеру
         for (QuestionEntity entity : savedEntities) {
             ids[entity.getOriginalIndex() - 1] = entity.getId();
         }
@@ -167,11 +239,11 @@ public class TestService {
     }
 
 
-    public TestEntity addTest(TestRequestDTO testRequestDTO){
+    public TestEntity addTest(TestRequestDTO testRequestDTO) {
         return testRepository.save(testMapper.toTestEntity(testRequestDTO, generateQuestionsIds(testRequestDTO)));
     }
 
-    public QuestionsResponseDTO getAllQuestions(){
+    public QuestionsResponseDTO getAllQuestions() {
         return new QuestionsResponseDTO(testMapper.toQuestionModelList(questionRepository.findAll()));
     }
 }
