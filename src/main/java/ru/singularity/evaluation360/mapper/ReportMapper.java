73c268d6d3148b3dfad008c5c31a67e5307d67e5
daniil <daniltihomirov946@gmail.com>
2025-04-22 15:00:22 +0300

    small fix

--- src/main/java/ru/singularity/evaluation360/mapper/ReportMapper.java
+++ src/main/java/ru/singularity/evaluation360/mapper/ReportMapper.java
@@ -1,9 +1,6 @@
 package ru.singularity.evaluation360.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.Mappings;
+import org.mapstruct.*;
 import ru.singularity.evaluation360.dto.result.ResultRequestDTO;
 import ru.singularity.evaluation360.entity.ReportEntity;
 
@@ -16,5 +13,12 @@ public interface ReportMapper {
             @Mapping(target = "testId", source = "testId")
     })
     ReportEntity toReportEntity(ResultRequestDTO resultRequestDTO, String testId);
+
+    @AfterMapping
+    default void setGeneratedIndex(@MappingTarget ReportEntity report,
+                                   ResultRequestDTO dto,
+                                   String testId) {
+        report.setId(dto.evaluatedId() + testId + dto.evaluatorId());
+    }
 
 }
