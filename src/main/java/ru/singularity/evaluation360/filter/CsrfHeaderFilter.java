88dd2876683179d11b629fbe1f9238ff1ee82d75
Med77_1 <mm7737333@gmail.com>
2025-05-26 12:01:28 +0300

    feat: Update CsrfHeaderFilter
    update filter

--- src/main/java/ru/singularity/evaluation360/filter/CsrfHeaderFilter.java
+++ src/main/java/ru/singularity/evaluation360/filter/CsrfHeaderFilter.java
@@ -1,6 +1,5 @@
 package ru.singularity.evaluation360.filter;
 
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,7 +21,14 @@ public class CsrfHeaderFilter extends OncePerRequestFilter {
 
         CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
         if (csrf != null) {
-            response.setHeader("X-CSRF-TOKEN", csrf.getToken());
+            String token = csrf.getToken();
+
+            response.setHeader("Set-Cookie", String.format("XSRF-TOKEN=%s; Path=/; HttpOnly=false; SameSite=Strict", token));
+
+            response.setHeader("X-CSRF-TOKEN", token);
+            response.setHeader("X-XSRF-TOKEN", token);
+            response.setHeader("X-CSRF-HEADER", csrf.getHeaderName());
+            response.setHeader("X-CSRF-PARAM", csrf.getParameterName());
         }
         filterChain.doFilter(request, response);
     }
